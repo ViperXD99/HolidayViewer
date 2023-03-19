265aab6818d10ab23a7a687a2c8dd048f3739e66
fshazn <shazna464@gmail.cpm>
2023-03-19 12:14:28 +0530

    Local Holiday Layout with Recycler Views

---
+++ app/src/main/java/lk/nibm/holidayviewer/GlobalHolidays.kt
@@ -0,0 +1,11 @@
+package lk.nibm.holidayviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GlobalHolidays : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_global_holidays)
+    }
+}
